e0a12979ca274d48ea7a69ef72b86ea5a2f3fbf7
Markus Hackspacher <hackspacher@gmx.de>
2019-01-17 10:34:13 +0100

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -48,114 +48,41 @@ class TestCodeFormat(unittest.TestCase):
         """
         ed_user = model.User(name='Ed', lastname='Jones')
         self.session.add(ed_user)
-        our_user = self.session.query(model.User).filter_by(name='ed').first()
+        our_user = self.session.query(model.User).filter_by(name='Ed').first()
         self.assertEqual(our_user.name, 'Ed')
         self.assertEqual(our_user.lastname, 'Jones')
 
-    def test_team_model(self):
-        """Test the user model
+    def test_age_model(self):
+        """Test the age model
         """
-        teamlist = ['Germany', 'Brasil', 'Italy', 'Spain']
-        for te in teamlist:
-            self.session.add(model.Team(name=te))
-        our_team = self.session.query(model.Team).filter_by(
-                name='Germany').first()
-        self.assertEqual(our_team.name, 'Germany')
-        self.assertEqual(our_team.team_bets, [])
-        self.assertEqual(our_team.cup_winner_bets, [])
-        self.assertEqual(our_team.competitions, [])
-        teams = self.session.query(model.Team).all()
-        self.assertEqual(teams[2].name, 'Italy')
-        self.assertEqual(teams[3].name, 'Spain')
+        session.add(model.Age(name='young talented', short='advanced'))
+        our_team = self.session.query(model.Age).filter_by(
+                name='young talented').first()
+        self.assertEqual(our_team.name, 'young talented')
+        self.assertEqual(our_team.short , 'advanced')
+        self.assertEqual(our_team.sep, 1)
+        self.assertEqual(our_team.adult, 1)
+        self.assertEqual(our_team.sorting , 0)
 
-    def test_competition_model(self):
-        """Test the Competition model
+    def test_bow_model(self):
+        """Test the bow model
         """
-        testcompetition = model.Competition(name='World Cup',
-                                            cup_winner_id=2,
-                                            rule_right_winner=1,
-                                            rule_right_goaldif=2,
-                                            rule_right_result=3,
-                                            rule_cup_winner=10)
-        self.session.add(testcompetition)
-        our_competition = self.session.query(model.Competition).first()
-        self.assertEqual(our_competition.name, 'World Cup')
-        self.assertEqual(our_competition.cup_winner_id, 2)
-        self.assertEqual(our_competition.rule_right_winner, 1)
-        self.assertEqual(our_competition.rule_right_goaldif, 2)
-        self.assertEqual(our_competition.rule_right_result, 3)
-        self.assertEqual(our_competition.rule_cup_winner, 10)
-        self.test_team_model()
-        our_competition = self.session.query(model.Competition).first()
-        self.assertEqual(our_competition.name, 'World Cup')
-        self.assertEqual(our_competition.teams.name, 'Brasil')
+        session.add(model.Bow(name='long bow', short='long'))
+        our_team = self.session.query(model.Bow).filter_by(
+                name='long bow').first()
+        self.assertEqual(our_team.name, 'long bow')
+        self.assertEqual(our_team.short , 'long')
+        self.assertEqual(our_team.sorting , 0)
 
-    def test_cupwinnerbet_model(self):
-        """Test the CupWinnerBet model
+    def test_club_model(self):
+        """Test the club model
         """
-        self.test_competition_model()
-        self.test_user_model()
-        ed_winner = model.CupWinnerBet(competition_id=1, team_id=1, user_id=1)
-        self.session.add(ed_winner)
-        our_cupwinnerbet = self.session.query(model.CupWinnerBet).first()
-        self.assertEqual(our_cupwinnerbet.name, 'ed World Cup Germany')
-        self.assertEqual(our_cupwinnerbet.teams.name, 'Germany')
-        self.assertEqual(our_cupwinnerbet.point, 0)
-
-    def test_game_model(self):
-        """Test the Game model
-        """
-        self.test_competition_model()
-        gametest = model.Game(
-                competition_id=1, team_home_id=2, team_away_id=3,
-                result_home=2, result_away=2)
-        self.session.add(gametest)
-        gametest = model.Game(competition_id=1, team_home_id=2, team_away_id=1,
-                              result_home=1, result_away=2)
-        self.session.add(gametest)
-        our_game = self.session.query(model.Game).first()
-        self.assertEqual(our_game.name, 'World Cup: Brasil:Italy 2:2')
-        self.assertEqual(our_game.start_date, None)
-
-    def test_gamebet_model(self):
-        """Test the GameBet model
-        """
-        self.test_user_model()
-        self.test_game_model()
-        gamebettest = model.GameBet(user_id=1, game_id=1,
-                                    bet_home=1, bet_away=1)
-        self.session.add(gamebettest)
-        gamebettest = model.GameBet(user_id=1, game_id=2,
-                                    bet_home=1, bet_away=2)
-        self.session.add(gamebettest)
-        gamebettest = model.GameBet(user_id=1, game_id=2,
-                                    bet_home=2, bet_away=2)
-        self.session.add(gamebettest)
-        gamebettest = model.GameBet(user_id=1, game_id=2,
-                                    bet_home=2, bet_away=1)
-        self.session.add(gamebettest)
-        gamebettest = model.GameBet(user_id=1, game_id=2,
-                                    bet_home=2, bet_away=5)
-        self.session.add(gamebettest)
-
-        our_user = self.session.query(model.User).filter_by(name='ed').first()
-        self.assertEqual(our_user.name, 'ed')
-        self.assertEqual(our_user.id, 1)
-        self.assertEqual(our_user.game_bets[0].name, 'Brasil:Italy 1:1')
-        self.assertEqual(our_user.game_bets[0].point, 2)
-        self.assertEqual(our_user.game_bets[1].name, 'Brasil:Germany 1:2')
-        self.assertEqual(our_user.game_bets[1].point, 3)
-        self.assertEqual(our_user.game_bets[2].point, 0)
-        self.assertEqual(our_user.game_bets[3].point, 0)
-        self.assertEqual(our_user.game_bets[4].point, 1)
-        self.assertEqual(sum(x.point for x in our_user.game_bets), 6)
-        with self.assertRaises(IndexError):
-            our_user.game_bets[5].name
-
-        our_bet = self.session.query(model.GameBet).all()
-        self.assertEqual(our_bet[0].point, 2)
-        self.assertEqual(our_bet[1].point, 3)
-        self.assertEqual(our_bet[2].point, 0)
-        self.assertEqual(our_bet[3].point, 0)
-        self.assertEqual(our_bet[4].point, 1)
-        self.assertEqual(sum(x.point for x in our_bet), 6)
+        session.add(model.Club(name='long bow club', short='long'))
+        our_team = self.session.query(model.Club).filter_by(
+                name='long bow club').first()
+        self.assertEqual(our_team.name, 'long bow club')
+        self.assertEqual(our_team.short , 'long')
+        self.assertEqual(our_team.email  , '')
+        self.assertEqual(our_team.address  , '')
+        self.assertEqual(our_team.payment  , 0)
+        self.assertEqual(our_team.advertising  , 0)
