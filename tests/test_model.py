0cb54fb7e1f48f0b604d8bfebd74e329a00474e4
Markus Hackspacher <hackspacher@gmx.de>
2019-01-17 10:54:27 +0100

    Set all model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# archerrang2
+"""
+Archerank2
 
-# Copyright (C) <2019> Markus Hackspacher
+Copyright (C) <2018> Markus Hackspacher
 
-# This file is part of archerrang2.
+This file is part of Archerank2.
 
-# archerrang2 is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+Archerank2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-# archerrang2 is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
+Archerank2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with archerrang2.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with Archerank2.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import unittest
-from sqlalchemy import orm
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, orm
+
 from modules import model
 
 
@@ -55,34 +57,43 @@ class TestCodeFormat(unittest.TestCase):
     def test_age_model(self):
         """Test the age model
         """
-        session.add(model.Age(name='young talented', short='advanced'))
-        our_team = self.session.query(model.Age).filter_by(
+        self.session.add(model.Age(name='young talented', short='advanced'))
+        our_age = self.session.query(model.Age).filter_by(
                 name='young talented').first()
-        self.assertEqual(our_team.name, 'young talented')
-        self.assertEqual(our_team.short , 'advanced')
-        self.assertEqual(our_team.sep, 1)
-        self.assertEqual(our_team.adult, 1)
-        self.assertEqual(our_team.sorting , 0)
+        self.assertEqual(our_age.name, 'young talented')
+        self.assertEqual(our_age.short , 'advanced')
+        self.assertEqual(our_age.sep, 1)
+        self.assertEqual(our_age.adult, 1)
+        self.assertEqual(our_age.sorting , 0)
 
     def test_bow_model(self):
         """Test the bow model
         """
-        session.add(model.Bow(name='long bow', short='long'))
-        our_team = self.session.query(model.Bow).filter_by(
+        self.session.add(model.Bow(name='long bow', short='long'))
+        our_bow = self.session.query(model.Bow).filter_by(
                 name='long bow').first()
-        self.assertEqual(our_team.name, 'long bow')
-        self.assertEqual(our_team.short , 'long')
-        self.assertEqual(our_team.sorting , 0)
+        self.assertEqual(our_bow.name, 'long bow')
+        self.assertEqual(our_bow.short , 'long')
+        self.assertEqual(our_bow.sorting , 0)
 
     def test_club_model(self):
         """Test the club model
         """
-        session.add(model.Club(name='long bow club', short='long'))
-        our_team = self.session.query(model.Club).filter_by(
+        self.session.add(model.Club(name='long bow club', short='long'))
+        our_club = self.session.query(model.Club).filter_by(
                 name='long bow club').first()
-        self.assertEqual(our_team.name, 'long bow club')
-        self.assertEqual(our_team.short , 'long')
-        self.assertEqual(our_team.email  , '')
-        self.assertEqual(our_team.address  , '')
-        self.assertEqual(our_team.payment  , 0)
-        self.assertEqual(our_team.advertising  , 0)
+        self.assertEqual(our_club.name, 'long bow club')
+        self.assertEqual(our_club.short , 'long')
+        self.assertEqual(our_club.email  , '')
+        self.assertEqual(our_club.address  , '')
+        self.assertEqual(our_club.payment  , 0)
+        self.assertEqual(our_club.advertising  , 0)
+
+    def test_setting_model(self):
+        """Test the setting model
+        """
+        self.session.add(model.Setting(name='lastlayout', value='long'))
+        our_set = self.session.query(model.Setting).filter_by(
+                name='lastlayout').first()
+        self.assertEqual(our_set.name, 'lastlayout')
+        self.assertEqual(our_set.value  , 'long')
