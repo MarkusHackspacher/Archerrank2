18780c8d116f5bf17cdbe4f53bdd23a2116f196f
Markus Hackspacher <hackspacher@gmx.de>
2023-07-26 14:20:04 +0200

    Update test_PrintDialog.py

--- tests/test_PrintDialog.py
+++ tests/test_PrintDialog.py
@@ -2,22 +2,22 @@ import unittest
 from modules.gui.printdialog import DlgPrint
 
 
-def test_handle_print(qtbot):
-    dialog = DlgPrint()
-    qtbot.addWidget(dialog)
-    editor = dialog.editor
-    editor.setText('This is a test')
-    dialog.handle_print()
-    assert dialog.buttonPrint.isEnabled()
 
+class TestDlgPrint(unittest.TestCase):
 
-def test_handle_preview(qtbot):
-    dialog = DlgPrint()
-    qtbot.addWidget(dialog)
-    editor = dialog.editor
-    editor.setText('This is a test')
-    dialog.handle_preview()
-    assert dialog.buttonPreview.isEnabled()
+    def test_handle_print(self):
+        dialog = DlgPrint()
+        editor = dialog.editor
+        editor.setText('This is a test')
+        dialog.handle_print()
+        self.assertTrue(dialog.buttonPrint.isEnabled())
+
+    def test_handle_preview(self):
+        dialog = DlgPrint()
+        editor = dialog.editor
+        editor.setText('This is a test')
+        dialog.handle_preview()
+        self.assertTrue(dialog.buttonPreview.isEnabled())
 
 if __name__ == "__main__":
     unittest.main()
