27ac24aa372b8f76f41b33880c6db1a0ff831a5c
Markus Hackspacher <hackspacher@gmx.de>
2023-07-26 13:32:54 +0200

    Update test_MainWindow.py

--- tests/test_MainWindow.py
+++ tests/test_MainWindow.py
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Archerank2
+
+Copyright (C) <2019,2023> Markus Hackspacher
+
+This file is part of Archerank2.
+
+Archerank2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Archerank2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Archerank2.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import unittest
-from mainwindow import MainWindow
+from modules.gui.mainwindow import MainWindow
 
 
 class TestMainWindow(unittest.TestCase):
